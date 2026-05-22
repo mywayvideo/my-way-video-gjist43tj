ec591458e0ac8cdb3c223754f2cfcebcb3b0f31e
Skip <skip@usecurling.com>
2026-05-22 00:59:24 +0000

    Auto-commit after full sync (build simulation)

--- supabase/functions/execute_ai_search_v2/index.ts
+++ supabase/functions/execute_ai_search_v2/index.ts
@@ -4,7 +4,8 @@ import { loadCacheSettings } from '../../_shared/cacheSettings.ts'
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers':
+    'authorization, x-client-info, apikey, content-type, x-request-id',
 }
 
 // =========================
